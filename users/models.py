1f4299f9d69ee2a6c593a4d725747a6dc9ef46a0
георгий савинцев <igsgo4645@gmail.com>
2024-10-09 21:44:38 +0400

    add app users - create model, view(generics), serializers, permissions, migrations

---
+++ users/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите пользователя"
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        blank=True,
+        null=True,
+        help_text="Укажите номер телефона",
+    )
+    tg_nickname = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        help_text="Укажите ник в телеграмм",
+        verbose_name="Имя в ТГ",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True,
+        null=True,
+        help_text="Загрузите аватар",
+        verbose_name="Аватар",
+    )
+    tg_chat_id = models.CharField(max_length=35, blank=True, null=True, verbose_name="telegram chat id", help_text="Укажите chat id в телеграмм")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
