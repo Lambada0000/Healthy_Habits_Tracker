f43a7a9e6f24daff6e101cd5537f57307f4365c7
георгий савинцев <igsgo4645@gmail.com>
2024-10-12 12:59:26 +0400

    fix urls and add validators

--- habits/urls.py
+++ habits/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
 from habits.apps import HabitsConfig
 from habits.views import (HabitCreateAPIView, HabitDestroyAPIView,
-                          HabitListAPIView, HabitRetrieveAPIView,
-                          HabitUpdateAPIView)
+                          HabitListAPIView, HabitPublishedListAPIView,
+                          HabitRetrieveAPIView, HabitUpdateAPIView)
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
-    path('habits/', HabitListAPIView.as_view(), name='habit_list'),
-    path('habits/<int:pk>/', HabitRetrieveAPIView.as_view(), name='habit_detail'),
-    path('habits/create/', HabitCreateAPIView.as_view(), name='habit_create'),
-    path('habits/<int:pk>/update/', HabitUpdateAPIView.as_view(), name='habit_update'),
-    path('habits/<int:pk>/delete/', HabitDestroyAPIView.as_view(), name='habit_delete'),
+    path("habits/", HabitListAPIView.as_view(), name="habit_list"),
+    path("habits/<int:pk>/", HabitRetrieveAPIView.as_view(), name="habit_detail"),
+    path("habits/create/", HabitCreateAPIView.as_view(), name="habit_create"),
+    path("habits/<int:pk>/update/", HabitUpdateAPIView.as_view(), name="habit_update"),
+    path("habits/<int:pk>/delete/", HabitDestroyAPIView.as_view(), name="habit_delete"),
+    path(
+        "habits_published/",
+        HabitPublishedListAPIView.as_view(),
+        name="habits_published",
+    ),
 ]
