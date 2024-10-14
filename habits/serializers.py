726fb71d13e8d1b51cea0133bb28af7d85f49b15
георгий савинцев <igsgo4645@gmail.com>
2024-10-14 11:08:35 +0400

    add validators

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework.serializers import ModelSerializer
 from habits.models import Habit
+from habits.validators import (
+    ChoiceRewardValidator,
+    DurationValidator,
+    PleasantHabitValidator,
+    PeriodicityValidator,
+    AbsenceValidator,
+)
 
 
 class HabitSerializer(ModelSerializer):
     class Meta:
         model = Habit
         fields = "__all__"
+
+    def validate(self, data):
+        ChoiceRewardValidator("related_habit", "reward")(data)
+        DurationValidator("duration")(data)
+        PleasantHabitValidator("related_habit", "pleasant_habit_sign")(data)
+        PeriodicityValidator("periodicity")(data)
+        AbsenceValidator("reward", "related_habit", "pleasant_habit_sigh")(data)
+        return data
